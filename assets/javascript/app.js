47ba9b6e8405991508e1b1a913d80d54884322da
Michelangelo <youngbrennin20@gmail.com>
2018-06-22 15:55:56 -0700

    99% done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,35 +11,44 @@
 
   var database = firebase.database();
 // start of javascript coding
-var name = [];
-var place = [];
-var TrainTime = [];
-var monthsWorked = [];
-var freq = [];
-var billed = [];
+
 //Capture Button Click
 $("#add-user").on("click", function (event) {
     event.preventDefault();
     name = $("#name-input").val().trim();
-        console.log(name)
+        console.log(name);
+
     place = $("#place-input").val().trim();
         console.log(place);
-    TrainTime = $("#TrainTime-input").val().trim();
-        console.log(TrainTime);
+
+    TrainTime = $("#TrainTime-input").val().trim(); 
+    var frmt = moment(TrainTime, "hmm").format("HH:mm"); //=== "01:23"
+        TrainTime = frmt;
+
     freq = $("#freq-input").val().trim();
         console.log(freq);
-//sends the information to firebase
+
+    minutesAway = $('#TrainTime-input').val().trim();
+    var convertIt = moment(minutesAway).subtract(1, "years");
+    var currentTime = moment();
+    moment(currentTime).format("hh:mm")
+    var diffTime = moment().diff(moment(convertIt), "minutes");//sends the information to firebase
+    var tRemainder = diffTime % freq;
+    var tMinutesTillTrain = freq - tRemainder;
+    minutesAway = tRemainder
+    console.log(minutesAway);
     database.ref().push({
         name: name,
         place: place,
         TrainTime: TrainTime,
-        freq: freq
+        freq: freq,
+        minutesAway: minutesAway
     });
  
 });
 //displays the value typed in into the current employees box
     database.ref().on("child_added", function (snapshot){
-        $("#add-employee-row").append("<tr><td>" + snapshot.val().name +"</td><td>" + snapshot.val().place + "</td><td>" + snapshot.val().freq + "</td><td>" + snapshot.val().TrainTime + "</td></tr>");
+        $("#add-employee-row").append("<tr><td>" + snapshot.val().name +"</td><td>" + snapshot.val().place + "</td><td>" + snapshot.val().freq + "</td><td>" + snapshot.val().TrainTime + "</td><td>" + snapshot.val().minutesAway + "</td></tr>");
        
     });
         //need a seperate array for total billed
